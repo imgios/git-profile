c621e88147076904fa1697cc267d18d56cd9941b
Giosuè Sulipano <sulipano.gs@gmail.com>
2022-03-31 15:41:39 +0200

    Add script flags

--- git-profile.sh
+++ git-profile.sh
@@ -17,24 +17,47 @@ get_usage() {
 
 # This functions save the actual .gitconfig in profiles/default.gitconfig
 save_gitconfig() {
-  if (cp ~/.gitconfig $PROFILE_DIR/default.gitconfig); then
+  if cp ~/.gitconfig $PROFILE_DIR/default.gitconfig; then
     echo "INFO: Profile saved in $PROFILE_DIR/default.gitconfig"
-  elif
+    return 0
+  else
     echo "ERROR: Profile not saved! Be sure the profiles path exists in $PROFILE_DIR"
-    exit 1
+    return 1
   fi
 }
 
-# See if the script has the profile name
+# Script flags
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -V | --version )
+    echo $VERSION
+    exit
+    ;;
+  -s | --save )
+    save_gitconfig
+    exit
+    ;;
+  -h | --help )
+    get_usage
+    exit
+    ;;
+  * )
+    echo "Flag $1 not recognised!"
+    get_usage
+    exit
+    ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+
+# See if the script has at least a param
 if [[ $# -ne 1 ]]; then
     get_usage
     exit 1
 fi
 
 # Replace the Git config and check if it's ok or not
-if (cp $PROFILE_DIR/$1.gitconfig ~/.gitconfig); then
+if cp $PROFILE_DIR/$1.gitconfig ~/.gitconfig; then
   echo "INFO: Profile $1 applied!"
-elif
+else
   echo "ERROR: Profile $1 not applied! Be sure the file $1.gitconfig exists in $PROFILE_DIR"
   exit 1
 fi
