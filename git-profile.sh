72033615c9ad6a3fe84131b5e75db3e7b807db05
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2022-03-31 15:11:22 +0200

    Replace case-execution with direct execution
    A message error will be shown if the .gitconfig file doesn't exist.

--- git-profile.sh
+++ git-profile.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/bash
 
 # Switch Git Profile replacing the .gitconfig
-# Usage ./git-profile.sh <github/gitlab>
-# Where github/gitlab are .gitconfig files existing in the scriptdir/profiles
+# Usage ./git-profile.sh <profile-name>
+# Where <profile-name> is a <profile-name>.gitconfig files existing in the scriptdir/profiles path.
 
 # Func: Print USAGE
 get_usage() {
-  echo "USAGE: ${SCRIPT_NAME%%.*} {github|gitlab}"
+  echo "USAGE: ${SCRIPT_NAME%%.*} <profile-name>"
 }
 
+# See if the script has the profile name
 if [[ $# -ne 1 ]]; then
     SCRIPT_NAME=$(basename $0)
     get_usage
     exit 1
 fi
 
-case "$1" in
-    github | gitlab)
-        SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-        cp $SCRIPT_DIR/profiles/$1.gitconfig ~/.gitconfig
-        ;;
-    *)
-        get_usage
-        exit 1
-        ;;
-esac
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROFILE_DIR=$SCRIPT_DIR/profiles
+if (cp $PROFILE_DIR/$1.gitconfig ~/.gitconfig); then
+  echo "INFO: Profile $1 applied!"
+elif
+  echo "ERROR: Profile $1 not applied! Be sure the file $1.gitconfig exists in $PROFILE_DIR"
+  exit 1
+fi
