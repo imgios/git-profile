fb59eae914f85dd8d511540937280688770118e3
Giosuè Sulipano <sulipano.gs@gmail.com>
2022-03-31 15:34:17 +0200

    Add .gitconfig save function

--- git-profile.sh
+++ git-profile.sh
@@ -4,20 +4,33 @@
 # Usage ./git-profile.sh <profile-name>
 # Where <profile-name> is a <profile-name>.gitconfig files existing in the scriptdir/profiles path.
 
+# Variables
+VERSION=0.1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROFILE_DIR=$SCRIPT_DIR/profiles
+SCRIPT_NAME=$(basename $0)
+
 # This functions prints a USAGE-related string
 get_usage() {
   echo "USAGE: ${SCRIPT_NAME%%.*} <profile-name>"
 }
 
+# This functions save the actual .gitconfig in profiles/default.gitconfig
+save_gitconfig() {
+  if (cp ~/.gitconfig $PROFILE_DIR/default.gitconfig); then
+    echo "INFO: Profile saved in $PROFILE_DIR/default.gitconfig"
+  elif
+    echo "ERROR: Profile not saved! Be sure the profiles path exists in $PROFILE_DIR"
+    exit 1
+  fi
+}
+
 # See if the script has the profile name
 if [[ $# -ne 1 ]]; then
-    SCRIPT_NAME=$(basename $0)
     get_usage
     exit 1
 fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PROFILE_DIR=$SCRIPT_DIR/profiles
 # Replace the Git config and check if it's ok or not
 if (cp $PROFILE_DIR/$1.gitconfig ~/.gitconfig); then
   echo "INFO: Profile $1 applied!"
