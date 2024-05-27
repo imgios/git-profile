c7533dcd801d8d8f960837561ddc0b64b160d0cf
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-05-27 15:35:43 +0200

    suggest the profile name

--- git-profile.completion.sh
+++ git-profile.completion.sh
@@ -4,12 +4,23 @@ __git-profile_completion() {
 	local current previous options
 	COMPREPLY=()
 	current="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	previous="${COMP_WORDS[COMP_CWORD-1]}"
 	options="--directory --verbose"
+	commands=( "set" "save" "list" "help" "version" )
+	require_input=( "set" "save" )
 
 	if [[ ${current} == -* ]]; then
 		COMPREPLY=( $(compgen -W "${options}" -- ${current}) )
 		return 0
+	elif [[ "${require_input[*]}" =~ ${previous} ]]; then
+		profiles=""
+		if [[ ! -d "~/.gitprofile" ]]; then
+			profiles=$(ls ~/.gitprofile)
+		fi
+		COMPREPLY=($(compgen -W "${profiles}" "${current}"))
+		return 0
+	else
+		COMPREPLY=($(compgen -W "${commands[*]}" "${current}"))
 	fi
 }
 
